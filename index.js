33e5ff4ad31cee67a6f29535113094ea6beb52c2
canjianchen <55061748+canjianchen@users.noreply.github.com>
2020-03-09 19:25:27 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -6,5 +6,5 @@ const headline2 = document.querySelector('.headline2');
 const tl = new TimelineMax();
 
 tl.fromTo(hero,1.3,{height:"0"},{height:'90%',ease:Power2.easeInOut})
-.fromTo(hero,1.8,{width:"100%"},{width:'70%',ease:Power2.easeInOut})
+.fromTo(hero,1.8,{width:"50%"},{width:'100%',ease:Power2.easeInOut})
 .fromTo(slider,1.3,{x:"-100%"},{x:'0%',ease:Power2.easeInOut},"-=1.4");
